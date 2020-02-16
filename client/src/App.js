82ed619ffd9a2433357214f8dc262bd72481308f
Shadab Ali <shadabali604@gmail.com>
2020-02-16 19:45:02 +0530

    express router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Route, Router} from 'r';
-import join from './components/join';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import join from './components/Join';
 import Chat from './components/Chat';
 
 const App = () => (  
     <Router>
-    <Route Path='/' exact component={join} />
-    <Route Path='/chat' exact component={Chat} />
-
-    
+     <Route Path='/' exact component={join} />
+    <Route Path='/chat' component={Chat} />
     </Router>
     );
 export default App;
