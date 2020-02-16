af7ffab9335d8f76db37a7013023baa941734a11
Shadab Ali <shadabali604@gmail.com>
2020-02-16 19:54:26 +0530

    update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+console.log('We have new connection!!!')
+});
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
